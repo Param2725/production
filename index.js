1b0862135c502b2698f6e7b811574fa49ba8faa3
Param Patel <parampatel2725@gmail.com>
2025-06-28 18:20:08 +0530

    new update

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ app.get('/amogh', (req, res) => {
     res.send('vaghri is a great place to learn and grow!');
 });
 
+app.get('/parasher' , (req , res) => {
+    res.send('parasher is pariyo');
+});
+
+app.get('/darshit' , (req , res) => {
+    res.send('rono rona ni rite');
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`App is listening on port ${process.env.PORT}`);
 });
